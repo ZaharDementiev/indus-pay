6bf44238c89dded084b41bf41d4594260b8769b2
ZaharDem <zahardementiev@gmail.com>
2020-11-07 21:26:18 +0200

    pagination

--- app/UserRequest.php
+++ app/UserRequest.php
@@ -10,6 +10,8 @@ class UserRequest extends Model
     public const ACCEPTED = 1;
     public const DENIED = 2;
 
+    public const REQUESTS_ON_PAGE = 20;
+
     public function user()
     {
         return $this->belongsTo(User::class);
